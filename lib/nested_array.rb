2193de8e75d261c0927de8ec0875c19288926811
Nate Montgomery <kilikalbright@hotmail.com>
2019-12-08 21:14:20 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -38,6 +38,6 @@ end
 def matrix_update(matrix, row, column, new_value)
   # Update the matrix location at row and column to have the value of new_value
   # Return the updated matrix
-  ENNET_HOUSE[2] = "Tiny Ewell"
+  ENNET_HOUSE[0][2] = "Tiny Ewell"
   ENNET_HOUSE
 end
